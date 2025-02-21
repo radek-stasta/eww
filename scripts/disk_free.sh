f5d87704fa44d6e240317b22c500a6575d51fe83
Radek Šťasta <radek.stasta@gmail.com>
2025-02-21 09:51:50 +0100

    Fixed disk scripts

--- scripts/disk_free.sh
+++ scripts/disk_free.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Get the available free space in 1K blocks for /dev/disk/by-label/fedora
-available_kb=$(df --output=avail /dev/disk/by-label/fedora | tail -n 1)
+# Get the available free space in 1K blocks for root partition
+available_kb=$(df --output=avail / | tail -n 1)
 
 # Convert the available space to gigabytes with one decimal place
 available_gb=$(awk "BEGIN {printf \"%.1f\", $available_kb/1024/1024}")
--- scripts/disk_used_percentage.sh
+++ scripts/disk_used_percentage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Get the available free space in 1K blocks for /dev/disk/by-label/fedora
-available_kb=$(df --output=avail /dev/disk/by-label/fedora | tail -n 1)
+# Get the available free space in 1K blocks for root partition
+available_kb=$(df --output=avail / | tail -n 1)
 
-# Get total disk space in 1K blocks for /dev/disk/by-label/fedora
-total_kb=$(df --output=size /dev/disk/by-label/fedora | tail -n 1)
+# Get total disk space in 1K blocks for root partition
+total_kb=$(df --output=size / | tail -n 1)
 
 # Calculate used space
 used_kb=$(expr $total_kb - $available_kb)
